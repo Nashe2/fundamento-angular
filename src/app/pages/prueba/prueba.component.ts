1d824f2861c3a27b3c0e52a8746da3fe1a3d52d9
Nashe Osorio <nasheog2@gmail.com>
2024-05-05 23:10:22 -0600

    Property Binding.
    - Se usan [ ]
    
    - Necesario para configurar propiedades de los elementos (atributos) html.
    
    - Se utilizan para controlar dinamicamente algunos atributos de html para que sean renderizados.
    
    - Funcionan desde component.ts al objeto destino (atributo html), a esto se le conoce como flujo de datos.

--- src/app/pages/prueba/prueba.component.ts
+++ src/app/pages/prueba/prueba.component.ts
@@ -4,13 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-prueba',
   standalone: true,
   imports: [],
-  templateUrl: './prueba.component.html',/* Esto conecta al componente con el html, es decir esa es la vista asociada a este componente*/
-  styleUrl: './prueba.component.scss'
+  templateUrl:
+    './prueba.component.html' /* Esto conecta al componente con el html, es decir esa es la vista asociada a este componente*/,
+  styleUrl: './prueba.component.scss',
 })
 
 /* Aquí se pone la lógica del negocio */
 export class PruebaComponent {
-name = 'Nashe';
-age = 18;
-img = 'https://www.w3schools.com/howto/img_avatar.png';
+  name = 'Nashe';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+
+  btndisabled = false;
+
+  person = {
+    name: 'Nashe',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
 }
