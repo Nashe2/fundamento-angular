29a17c5cbca2c57a316b1e55038b60c309809278
Nashe Osorio <nasheog2@gmail.com>
2024-05-07 17:45:02 -0600

    Producto.
    - Cree un nuevo modulo llamado producto.
    
    - Agregue la ruta producto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PruebaComponent } from './pages/prueba/prueba.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AbarroteComponent } from './pages/abarrote/abarrote.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'abarrote',
     component: AbarroteComponent,
+  },
+  {
+    path: 'producto',
+    component: ProductoComponent,
   }
 ];
 
