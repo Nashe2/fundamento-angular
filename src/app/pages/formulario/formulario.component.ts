1902f11e6d9a67d528f01018eba61f7622de4f9b
Nashe Osorio <nasheog2@gmail.com>
2024-05-08 13:29:34 -0600

    Formulario.
    - Cree la estructura HTML del formulario.
    
    - Importe FormControl para que reconozca el ngModel.
    
    - El input necesita un name y que sea required para identificar el nombre, email y password.

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
