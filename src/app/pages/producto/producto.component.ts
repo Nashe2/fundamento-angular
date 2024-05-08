fbc59a027aff94c4ea4a2b2f1377545000c964a2
Nashe Osorio <nasheog2@gmail.com>
2024-05-08 11:37:21 -0600

    ngFor Object.
    - Cree una carpeta en assets y le agregue imagenes.
    
    - Cree un ngFor para mostrar la lista de objetos.
    
    - Uso solo la variable temporal y el nombre del array, dentro de ngFor.
    
    - Dentro del div uso string interpolation para llamar sus precios y sus imagenes.

--- src/app/pages/producto/producto.component.ts
+++ src/app/pages/producto/producto.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './producto.component.html',
-  styleUrl: './producto.component.scss'
+  styleUrl: './producto.component.scss',
 })
 export class ProductoComponent {
+  /*  */
+  producto = {
+  name: 'mouse',
+  price: '359' ,
+  image: './assets/images/toy.jpg',
+}
 
+  productos = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg',
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg',
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg',
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg',
+    },
+    {
+      name: 'Gafas',
+      price: 334,
+      image: './assets/images/glasses.jpg',
+    },
+  ];
 }
